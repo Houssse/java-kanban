bf8491ca48ef62eaf1b20aa7d68568e9e23a109c
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-09-13 15:24:32 +0300

    improved class task

--- src/Task.java
+++ src/Task.java
@@ -1,16 +1,11 @@
 public class Task {
-    private int id;
-    private String name;
-    private String description;
-    private Status status;
+    protected int id;
+    protected String title;
+    protected String description;
+    protected Status status;
 
-    public Task(int id, String name, String description, Status status) {
-        // Добавляем валидацию и в конструктор
-        if (name == null || name.isBlank()) {
-            System.out.println("Имя не может быть пустым");
-        }
-        this.id = id;
-        this.name = name;
+    public Task(String title, String description, Status status) {
+        this.title = title;
         this.description = description;
         this.status = status;
     }
@@ -19,38 +14,40 @@ public class Task {
         return id;
     }
 
-    public void setName(String name) {
-        if (name == null || name.isBlank()) {
-            System.out.println("Имя не может быть пустым");
-        }
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Status getStatus() {
         return status;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "Task{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
                 ", status=" + status +
                 '}';
     }
